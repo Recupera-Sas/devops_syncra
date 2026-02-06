5e886199aebc3f2579221048509bda60a4d9d595
JU4N D3V <161253541+JU4ND4V1DMM@users.noreply.github.com>
2026-02-06 15:13:05 -0500

    Se agrega lógica para procesar más hojas en exclusiones de Claro

--- modules/exclusions.py
+++ modules/exclusions.py
@@ -3,20 +3,42 @@ from datetime import datetime
 import pandas as pd
 
 def process_xlsx_file(input_file, output_path):
-    
+
     output_path = f"{output_path}/"
     DataFrame = pd.read_excel(input_file, sheet_name="EXCLUSIONES", dtype=str)
+    df = pd.ExcelFile(input_file)
+    list_sheet_names = df.sheet_names
+
+    if "EXCLUSIONES" in list_sheet_names:
+        extract_values_sheet_aditional(DataFrame, "EXCLUSIONES", output_path, len(list_sheet_names))
     
+    if len(list_sheet_names) > 1:
+        for sheet in list_sheet_names:
+            if sheet.strip().upper() != "EXCLUSIONES":
+                print(f"✅ Procesando hoja adicional: {sheet}")
+                df_additional = pd.read_excel(input_file, sheet_name=sheet, dtype=str)
+                try:
+                    extract_values_sheet_aditional(df_additional, sheet, output_path, len(list_sheet_names))
+                except Exception as e:
+                    print(f"💥 Error al procesar la hoja {sheet}: {e}")
+    
+    
+
+def extract_values_sheet_aditional(DataFrame, sheet_name, output_path, numeric_sheets):
+
     Columns_DF = ['DOCUMENTO', 'CUENTA', 'MINS', 'EMAILS']
     Characters = [' ', '<', '>']
     
     for column in Columns_DF:
         
-        for char in Characters:
-            DataFrame[column] = DataFrame[column].str.replace(char, '', regex=True)
-        
-        if column in ['DOCUMENTO', 'CUENTA', 'MINS']:
-            DataFrame[column] = DataFrame[column].str.replace(r'[^0-9]', '', regex=True)
+        try:
+            for char in Characters:
+                DataFrame[column] = DataFrame[column].str.replace(char, '', regex=True)
+            
+            if column in ['DOCUMENTO', 'CUENTA', 'MINS']:
+                DataFrame[column] = DataFrame[column].str.replace(r'[^0-9]', '', regex=True)
+        except Exception as e:
+            DataFrame[column] = None
     
     DataFrame1 = DataFrame[DataFrame['MINS'].notna() & (DataFrame['MINS'] != '')]
     DataFrame2 = DataFrame[DataFrame['DOCUMENTO'].notna() & (DataFrame['DOCUMENTO'] != '')]
@@ -25,7 +47,10 @@ def process_xlsx_file(input_file, output_path):
     
     Time_Value = datetime.now().strftime("%Y-%m-%d")
     
-    output_path = f"{output_path}---- Bases para CARGUE ----"
+    if numeric_sheets > 1:
+        output_path = f"{output_path}---- Bases para CARGUE ----/{sheet_name}"
+    else:
+        output_path = f"{output_path}---- Bases para CARGUE ----"
     
     if output_path not in os.listdir():
         os.makedirs(output_path, exist_ok=True)  # Ensure the directory exists
